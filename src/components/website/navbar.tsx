0491fd195ac179d4943b26386edf5306ff6aab48
Ori Avraham <mr.ori.avraham@gmail.com>
2024-05-09 22:46:50 +0300

    added a hero section to the website

--- src/components/website/navbar.tsx
+++ src/components/website/navbar.tsx
@@ -16,7 +16,7 @@ import ThemeToggle from "../shared/ThemeToggle";
 
 export function Navbar() {
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="#"
