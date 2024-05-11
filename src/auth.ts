aa434156ec90353fa1032100c68b0e6c548ef00d
Ori Avraham <mr.ori.avraham@gmail.com>
2024-05-11 21:44:46 +0300

    setup auth.js with prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Twitter from "next-auth/providers/twitter";
 import Zoom from "next-auth/providers/zoom";
+import prisma from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google, GitHub, Twitter, Zoom],
 });
